42dfc940ce4b9c00cd086651af29fedbb8fe9cbb
Calvin <calvinquach@gmail.com>
2017-09-20 13:02:54 -0400

    Inital files for binary and sample code.

---
+++ binary_images/main.cc
@@ -0,0 +1,34 @@
+
+#include <cstdio>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int
+main(int argc, char **argv){
+  
+  if (argc!=3) {
+    printf("Usage: %s file1 threshold file2\n", argv[0]);
+    return 0;
+  }
+  const string input_file(argv[1]);
+  const string threshold(argv[2]);
+  const string output_file(argv[3]);
+
+  Image an_image;
+  if (!ReadImage(input_file, &an_image)) {
+    cout <<"Can't open file " << input_file << endl;
+    return 0;
+  }
+
+  // Draw a line from (0, 0) to (100, 240) having gray-value
+  // 200.
+  DrawLine(0, 0, 100, 240, 200,
+	   &an_image); 
+  
+  if (!WriteImage(output_file, an_image)){
+    cout << "Can't write to file " << output_file << endl;
+    return 0;
+  }
+}
