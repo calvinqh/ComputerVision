323c65db02903b3c0efb484bc8a99419d5a45501
Calvin Quach <calvinquach@gmail.com>
2017-09-22 18:03:07 +0000

    Implement create binary image.

--- binary_images/programs.cc
+++ binary_images/programs.cc
@@ -9,10 +9,20 @@
 using namespace std;
 
 namespace Programs {
-  void createBinaryImage(Image *an_image, Image *out_image, unsigned long threshold) {
+  void createBinaryImage(Image *an_image, Image *out_image, size_t threshold) {
     cout << "Creating Binary Image....." << endl;
+    out_image->AllocateSpaceAndSetSize(an_image->num_rows(), an_image->num_columns());
+    out_image->SetNumberGrayLevels(an_image->num_gray_levels());
+    for(size_t i = 0; i< an_image->num_rows(); i++) {
+      for (size_t j = 0; j < an_image->num_columns(); j++) {
+        int byte = 0;
+        if(an_image->GetPixel(i,j) > threshold)
+          byte=225;
+        out_image->SetPixel(i,j,byte);
+      }
+    }
+    cout << out_image->num_gray_levels() << endl;
   }
-  void pz() {
-    cout << "Test" << endl;
-  }
+
+
 }
