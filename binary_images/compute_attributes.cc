5452903eb9fd2c78ada8ee9ecb8b2c6e00535036
Calvin Quach <calvinquach@gmail.com>
2017-09-24 22:41:33 +0000

    Compute Attribute now find the area and the center.

--- binary_images/compute_attributes.cc
+++ binary_images/compute_attributes.cc
@@ -3,13 +3,61 @@
 #include <cstdlib>
 #include <iostream>
 #include <string>
+#include <unordered_map>
+#include <vector>
+
 
 using namespace std;
 
 namespace Programs {
   
-  void ComputeAttributes(Image *labelImage, string database, Image *outImage) {
+  void ComputeAttributes(Image *label_image, string database, Image *out_image) {
+    out_image->AllocateSpaceAndSetSize(label_image->num_rows(), label_image->num_columns());
+    out_image->SetNumberGrayLevels(label_image->num_gray_levels());
+    
+    //maps labels to a list [area, sum of x, sum of y]
+    unordered_map<int, vector<size_t>> center_map;
+    vector<int> label_list;
+
+    //find the area and the sum of x's and y's 
+    for( size_t row = 0; row < label_image->num_rows(); row++ ) {
+      for( size_t col = 0; col < label_image->num_columns(); col++) {
+        int label = label_image->GetPixel(row,col);
+        if(label != 0) {
+          //if key does not exist
+          if(center_map.find(label) == center_map.end()){
+            vector<size_t> props = {1,col,row};
+            center_map[label] = props;
+            //label_list.push_back(label);
+          }
+          else {
+            center_map[label][0]++;
+            center_map[label][1] += col;
+            center_map[label][2] += row;
+          }
+        }
+      }
+    }
+    /* show areas, sum of x's and sum of y's MAKE SURE U ADD LABEL_LIST PUSHBACK IN CODE BEFORE
+    for(int i = 0; i < label_list.size(); i++) { 
+      cout << "Label: " << label_list[i] << endl;
+      for(int j = 0; j < 3; j++) {
+        cout << center_map[label_list[i]][j] << " ";
+      }
+      cout << endl << "-------" <<  endl;
+    }
+    */
+
+    //calculate the center
+    for(auto it = center_map.begin(); it != center_map.end(); ++it) {
+      center_map[it->first][1] /= center_map[it->first][0];
+      center_map[it->first][2] /= center_map[it->first][0];
+      cout << "Label: " << it->first << endl;
+      cout << "Area: " << center_map[it->first][0] << endl;
+      cout << "X: " << center_map[it->first][1] << endl;  
+      cout << "Y: " << center_map[it->first][2] << endl;  
+      cout << "----------------" << endl;
+    }
 
   }
-
 }
