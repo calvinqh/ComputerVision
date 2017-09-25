0fa646c3ac860111987fb10d03ce2a9fff90cafd
Calvin Quach <calvinquach@gmail.com>
2017-09-25 00:56:49 +0000

    Move create binary image implementation to seperate file.

--- binary_images/programs.cc
+++ binary_images/programs.cc
@@ -9,21 +9,6 @@
 using namespace std;
 
 namespace Programs {
-  void CreateBinaryImage(Image *an_image, Image *out_image, size_t threshold) {
-    cout << "Creating Binary Image ..." << endl;
-    out_image->AllocateSpaceAndSetSize(an_image->num_rows(), an_image->num_columns());
-    out_image->SetNumberGrayLevels(an_image->num_gray_levels());
-    for(size_t i = 0; i< an_image->num_rows(); i++) {
-      for (size_t j = 0; j < an_image->num_columns(); j++) {
-        int byte = 0;
-        if(an_image->GetPixel(i,j) > threshold)
-          byte=255;
-        out_image->SetPixel(i,j,byte);
-      }
-    }
-    cout << "Binary Image created." << endl;
-  }
-
 
   void CreateLabeledImage(Image *binary_image, Image *out_image) {
     cout << "Labeling Image ..." << endl;
