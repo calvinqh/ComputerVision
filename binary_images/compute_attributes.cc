65424d1c9b90b6cdd2a03cf3c62380d77dcb16c0
Calvin Quach <calvinquach@gmail.com>
2017-09-30 23:05:36 +0000

    Add roundness calculations and add it to the db file.

--- binary_images/compute_attributes.cc
+++ binary_images/compute_attributes.cc
@@ -17,7 +17,6 @@ namespace Programs {
     
     //maps labels to a list [area, sum of x, sum of y]
     unordered_map<int, vector<double>> center_map;
-    vector<int> label_list;
 
     //find the area and the sum of x's and y's 
     for( size_t row = 0; row < label_image->num_rows(); row++ ) {
@@ -28,7 +27,6 @@ namespace Programs {
           if(center_map.find(label) == center_map.end()){
             vector<double> props = {1,double(col),double(row)};
             center_map[label] = props;
-            //label_list.push_back(label);
           }
           else {
             center_map[label][0]++;
@@ -38,16 +36,7 @@ namespace Programs {
         }
       }
     }
-    /* show areas, sum of x's and sum of y's MAKE SURE U ADD LABEL_LIST PUSHBACK IN CODE BEFORE
-    for(int i = 0; i < label_list.size(); i++) { 
-      cout << "Label: " << label_list[i] << endl;
-      for(int j = 0; j < 3; j++) {
-        cout << center_map[label_list[i]][j] << " ";
-      }
-      cout << endl << "-------" <<  endl;
-    }
-    */
-
+   
     //calculate the center
     for(auto it = center_map.begin(); it != center_map.end(); ++it) {
       center_map[it->first][1] /= center_map[it->first][0];
@@ -59,14 +48,14 @@ namespace Programs {
       cout << "----------------" << endl;
     }
     //maps labels to their a,b,c paramets
-    //[a,b,c,theta,rho]
+    //[a,b,c,theta,roundness]
     unordered_map<int,vector<double>> parameters_map;
     for(size_t row = 0; row < label_image->num_rows(); row++){
       for(size_t col = 0; col < label_image->num_columns(); col++){
         int label = label_image->GetPixel(row,col);
         if(label != 0){
           if(parameters_map.find(label) == parameters_map.end()) {
-            vector<double> params = {0,0,0,0,0};
+            vector<double> params = {0,0,0,0,0,0};
             parameters_map[label] = params;
           }
           double x_center = center_map[label][1];
@@ -83,7 +72,7 @@ namespace Programs {
       }
     }
     
-    //calculate theta and rho and draw line
+    //calculate theta, rho, inertia, and roundness. Also draw line
     for(auto it = parameters_map.begin(); it != parameters_map.end(); ++it) {
       int label = it->first;
       double x_center = center_map[label][1];
@@ -94,6 +83,7 @@ namespace Programs {
       cout << "Label: " << label << endl;
       cout << "Params: " << a << ", " << b << ", " << c << endl;
       double tan_theta = atan2(b,a-c)/2;
+      double max_theta = tan_theta + (3.141592/2);
       cout << "Theta: " << tan_theta << endl;
       double rho = -1*((x_center*sin(tan_theta))-(y_center*cos(tan_theta)));
       cout << "Rho: " << rho << endl;
@@ -104,8 +94,15 @@ namespace Programs {
       double inertia = (a*pow(sin(tan_theta),2)) - (b*sin(tan_theta)*cos(tan_theta)) + (c*pow(cos(tan_theta),2));
       cout << "Inertia " << inertia << endl;
 
+      double max_inertia = (a*pow(sin(max_theta),2)) - (b*sin(max_theta)*cos(max_theta)) + (c*pow(cos(max_theta),2));
+      cout << "Max Inertia: " << max_inertia << endl;
+
+      double roundness = inertia/max_inertia;
+      cout << "Roundness: " << roundness << endl;
+
       parameters_map[label][3] = inertia;
       parameters_map[label][4] = tan_theta;
+      parameters_map[label][5] = roundness;
       double x1 = x_center+100;
       double y1 = ((x1*sin(tan_theta))+rho)/cos(tan_theta);
       //double x1 = x_center + (200*sin(tan_theta));
@@ -122,6 +119,8 @@ namespace Programs {
       cout << "Can't read file";
       return ;
     }
+    string header = "label | x center | y center | inertia | orientation | roundness\n";
+    fprintf(output, header.c_str());
     for(auto it = parameters_map.begin(); it != parameters_map.end(); it++) {
       string output_data = "";
       int label = it->first;
@@ -129,8 +128,9 @@ namespace Programs {
       int y_center = center_map[label][1];
       double inertia = parameters_map[label][3];
       double theta = parameters_map[label][4];
+      double roundness = parameters_map[label][5];
       output_data = to_string(label) + " " + to_string(x_center) + " " + to_string(y_center) + " ";
-      output_data += to_string(inertia) + " " + to_string(theta) + "\n";
+      output_data += to_string(inertia) + " " + to_string(theta) + " " + to_string(roundness) + "\n";
       cout << output_data << endl;
       fprintf(output, output_data.c_str());
     }
