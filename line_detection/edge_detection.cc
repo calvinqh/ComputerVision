b9763aa051a960016fa1e04ccf46ccb0aad7d2de
Calvin Quach <calvin@blocpower.org>
2017-10-31 14:32:36 -0400

    Implement GenerateEdgeStrength and CleanEdgeImage functions.

--- line_detection/edge_detection.cc
+++ line_detection/edge_detection.cc
@@ -31,5 +31,59 @@ namespace Programs {
     }
   }
 
+  void GenerateEdgeStrengthImage(Image* an_image, Image* strength_image) {
+    cout << "Generating Edge Strength Image." << endl;
+    //Setting up the mask
+    Image maskX;
+    Image maskY;
+    //The dimensions of the mask
+    int mask_r = 3;
+    int mask_c = 3;
+    //The mask values put into a 1D array (the length must match with mask_r * mask_c)
+    //TODO: make sure the mask is flipped!
+    vector<int> mask_array_X = 
+    [ 0,0,0,
+      1,1,1,
+      2,2,2 ];
+    vector<int> mask_array_Y = 
+    [ 0,0,0,
+      1,1,1,
+      2,2,2 ];
+    maskX.AllocateSpaceAndSetSize(mask_r,mask_c);
+    maskX.SetNumberGrayLevels(an_image->GetNumberGrayLevels());
+    maskY.AllocateSpaceAndSetSize(mask_r,mask_c);
+    maskY.SetNumberGrayLevels(an_image->GetNumberGrayLevels());
+    int counter = 0;
+    for(int i = 0; i < 3; i++) {
+      for(int j = 0; j < 3; j++) {
+        maskX.SetPixel(i,j,mask_array_X[counter]);
+        maskY.SetPixel(i,j,mask_array_Y[counter])
+        counter++;
+        cout << maskX.GetPixelSafe(i,j) << " ";
+      }
+      cout << endl;
+    }
+
+    //perform convolution on the input image and store it into the output image
+    convolve(&mask, an_image, out_image);
+    cout << "Edge Strength Image Created." << endl;
+  }
+
+
+  void CleanEdgeImage(Image* edge_image, int threshold, Image* out_image) {
+    cout << "Thresholding Edge Strength Image." << endl;
+    out_image->AllocateSpaceAndSetSize(edge_image->num_rows(), edge_image->num_columns());
+    out_image->SetNumberGrayLevels(edge_image->GetNumberGrayLevels());
+    for(int i = 0; i < edge_image->num_rows(); i++) {
+      for(int j = 0; j < edge_image->num_columns(); j++) {
+        int value = edge_image->GetPixel(i,j);
+        if(value >= threshold)
+          out_image.SetPixel(i,j,225);
+        else
+          out_image.SetPixel(i,j,0);
+      }
+    }
+    cout << "Thresholding Complete." << endl;
+  }
 
 }
