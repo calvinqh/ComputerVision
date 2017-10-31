02df5741a9cc27fdb2ae1d12100c7b71ec0731f8
Calvin Quach <calvin@blocpower.org>
2017-10-31 12:42:43 -0400

    Add hough line init implementation file.

---
+++ line_detection/hough_lines.cc
@@ -0,0 +1,24 @@
+#include "programs.h"
+#include "image.h"
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <math.h>
+
+using namespace std;
+
+namespace Programs {
+
+  vector<vector<int>> GenerateHoughSpace(Image* edge_image, Image* out_image) {
+    vector<vector<int>> voting_array;
+    return voting_array;
+  }
+
+  void LineDetection(Image* original_image, vector<vector<int>> voting_array, int threshold, Image* out_image) {
+
+  }
+
+}
