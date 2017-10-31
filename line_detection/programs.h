cf3cc692e3b9e5ab0e12eb2f085dbd7707a17a60
Calvin Quach <calvin@blocpower.org>
2017-10-31 14:31:49 -0400

    Fix function contracts in header and fix typo in line detection function header.

--- line_detection/programs.h
+++ line_detection/programs.h
@@ -24,15 +24,15 @@ namespace Programs {
   //@param strength_image: the edge image (with gray level correlating to the strength)
   void GenerateEdgeStrengthImage(Image* an_image, Image* strength_image);
 
-  //Thresholds the edge "strength" image, removes all weak edges
+  //Thresholds the edge "strength" image, removes all weak edges. The result is a binary edge image
   //@param edge_image: the edge strength image that the result is based off
   //@param threshold: gray level value that determines if pixel is an edge or not
-  //@param out_image: the applied threshold image for edge image
+  //@param out_image: the applied threshold image for edge image, results in a binary edge image
   void CleanEdgeImage(Image* edge_image, int threshold Image* out_image);
   
 
   //Creates the line hough space for the edge "strength" image provided
-  //@param edge_image: edge strength image for an image
+  //@param edge_image: binary edge image
   //@param out_image: saves the hough voting image produces from the edge image
   //@return: the 2d array for the voting space. rows are for theta, cols are for rho
   vector<vector<int>> GenerateHoughSpace(Image* edge_image, Image* out_image);
@@ -42,7 +42,7 @@ namespace Programs {
   //@param voting array: array containing the line votes from hough. array is indexed by [theta, rho]
   //@param threshold: the threshold on which votes in the voting array are considered lines
   //@param out_image: contains lines drawn onto original image 
-  void LineDetection(Image* original_image, vecotor<vector<int>> voting_array, int threshold, Image* out_image);
+  void LineDetection(Image* original_image, vector<vector<int>> voting_array, int threshold, Image* out_image);
 
 }	//namespace programs
 
