e38cec72c2201a775c3a31b370cdd3d4905a0c2d
Calvin Quach <calvin@blocpower.org>
2017-11-18 14:21:02 -0500

    Add generatebinary image and centerandradius locator headers.

--- orientation_reflectance/programs.h
+++ orientation_reflectance/programs.h
@@ -11,7 +11,23 @@ using namespace ComputerVisionProjects;
 
 
 namespace Programs {
+
+
+	//Create a binary image based off the threshold given
+	//@param an_image, the image that will be converted to a binary image
+	//@param threshold, the threshold in which a pixel will be considered apart of the object
+	//@param out_image, where the output binary image will be saved
+	void GenerateBinarayImage(Image* an_image, Image* out_image, int threshold);
+
+	//Finds the center and radius of the sphere from the given binary image
+	//@param binary_image, binary image of a sphere
+	//@return, vector of cetner and radius for given sphere binary image
+	//i.e, center X, center Y, radius
+	vector<int> FindCenterAndRadius(Image* binary_image)
+
 	
+
+
 }	//namespace programs
 
 
