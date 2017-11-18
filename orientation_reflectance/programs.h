972d66d79d7e7b6ffdd763780ea8d1fda4f7e889
Calvin Quach <calvin@blocpower.org>
2017-11-18 12:32:34 -0500

    Add base programs implementations

---
+++ orientation_reflectance/programs.h
@@ -0,0 +1,18 @@
+#ifndef _PROGRAMS_H_
+#define _PROGRAMS_H_
+
+#include "image.h"
+#include <cstdlib>
+#include <string>
+#include <vector>
+
+using namespace std;
+using namespace ComputerVisionProjects;
+
+
+namespace Programs {
+	
+}	//namespace programs
+
+
+#endif  
