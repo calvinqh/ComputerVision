456bfba619fc4eb896cdb2ac4d535b45348fb576
Calvin <calvinquach@gmail.com>
2017-11-19 21:58:37 -0500

    Create light sources implent file for functions used for s2.

---
+++ orientation_reflectance/light_sources.cc
@@ -0,0 +1,24 @@
+#include "programs.h"
+#include "image.h"
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <math.h>
+
+using namespace std;
+
+namespace Programs {
+
+  vector<int> FindLightSourceVector(Image* an_image) {
+    vector<int> light_source;
+
+    //find the brightest pixel
+    
+    return light_source;  
+  }
+
+
+}
