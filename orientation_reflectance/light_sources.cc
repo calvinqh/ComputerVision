b6cb9528764fe9efcc637cdd249de9e4c196581b
Calvin <calvinquach@gmail.com>
2017-11-24 10:39:27 -0500

    Fix program header in implementation file for FindLightSOurceVector and add comments on file to show what function are being implemented.

--- orientation_reflectance/light_sources.cc
+++ orientation_reflectance/light_sources.cc
@@ -1,3 +1,11 @@
+/*
+* Contains implementation of:
+*	
+*		vector<int> FindLightSourceVector(Image*,size_t,size_t)
+*
+*
+*
+*/
 #include "programs.h"
 #include "image.h"
 #include <cstdio>
@@ -12,12 +20,15 @@ using namespace std;
 
 namespace Programs {
 
-  vector<int> FindLightSourceVector(Image* an_image) {
+  vector<int> FindLightSourceVector(Image* an_image, size_t centerX, size_t centerY) {
     vector<int> light_source;
 
+    //find the brightest pixel location
     size_t maxBrightness = 0;
-    size_t maxR = 0, maxC = 0;
-    //find the brightest pixel
+    //maxR is the Y position
+    size_t maxR = 0;
+    //maxC is the X position
+    size_t maxC = 0;
     for(size_t i = 0; i < an_image->num_rowS(); i++) {
     	for(size_t j = 0; j < an_image->num_columns(); j++) {
     		int pixel = an_image->GetPixel(i,j);
