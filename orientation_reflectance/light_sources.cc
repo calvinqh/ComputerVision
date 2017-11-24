d7ae6335fc8958f72c196e37254a1998ce46be20
Calvin <calvinquach@gmail.com>
2017-11-24 10:32:55 -0500

    Find the highest pixel value in light souce picture for FindLightSourceVector().

--- orientation_reflectance/light_sources.cc
+++ orientation_reflectance/light_sources.cc
@@ -15,8 +15,21 @@ namespace Programs {
   vector<int> FindLightSourceVector(Image* an_image) {
     vector<int> light_source;
 
+    size_t maxBrightness = 0;
+    size_t maxR = 0, maxC = 0;
     //find the brightest pixel
-    
+    for(size_t i = 0; i < an_image->num_rowS(); i++) {
+    	for(size_t j = 0; j < an_image->num_columns(); j++) {
+    		int pixel = an_image->GetPixel(i,j);
+    		if(pixel > maxBrightness) {
+    			maxBrightness = pixel;
+    			maxR = i;
+    			maxC = j;
+    		}
+    	}
+    }
+
+
     return light_source;  
   }
 
