b217fe1a6fc489e828632c432189fcaef73e0297
Calvin <calvinquach@gmail.com>
2017-11-24 23:39:22 -0500

    Setup s3 driver with calls to findingsurface normal and reading the parameters.

--- orientation_reflectance/s3.cc
+++ orientation_reflectance/s3.cc
@@ -22,18 +22,55 @@ main(int argc, char **argv) {
     return 0;
   }
 
-  const string input_file(argv[1]);
-  const string output_file(argv[2]);
+  const string directions_file(argv[1]);
+  const string image1(argv[2]);
+  const string image2(argv[3]);
+  const string image3(argv[4]);
+  const size_t step = atoi(argv[5]);
+  const size_t threshold = atoi(argv[6]);
+  const string output_file(argv[7]);
 
-  Image an_image;
-  Image edge_image;
+  Image an_image1;
+  Image an_image2;
+  Image an_image3;
 
-  if (!ReadImage(input_file, &an_image)) {
-  	cout << "Can't open file " << input_file << endl;
-  	return 0;
+  //Load the image files
+  if (!ReadImage(image1, &an_image1)) {
+    cout << "Can't open file " << image1 << endl;
+    return 0;
+  }
+  if (!ReadImage(image2, &an_image2)) {
+    cout << "Can't open file " << image2 << endl;
+    return 0;
+  }
+  if (!ReadImage(image3, &an_image3)) {
+    cout << "Can't open file " << image3 << endl;
+    return 0;
   }
 
-  if (!WriteImage(output_file, edge_image)) {
+  //read parameter file
+  vector<vector<int>> directions;
+  ifstream file(directions_file);
+  string str;
+  int start = 0;
+  while(getline(file,str)) {
+    vector<int> source;
+    for(int i = 0; i < str.length(); i++) {
+      if(str[i] == ' ') {
+        string data = str.substr(start, i);
+        start = i + 1;
+        source.push_back(stoi(data));
+      }
+    }
+    directions.push_back(source);
+    start = 0;
+  }
+
+  //will contain image with surface normals drawn on
+  Image out_image;
+  FindSurfaceNormals(directions, &an_image1, &an_image2, &an_image3, step, threshold, &out_image);
+
+  if (!WriteImage(output_file, &out_image)) {
     cout << "Can't write to " << output_file << endl;
   }
   
