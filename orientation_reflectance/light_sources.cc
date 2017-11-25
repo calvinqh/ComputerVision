6365b23f945e29c661773184f9bcc21790677c5f
Calvin <calvinquach@gmail.com>
2017-11-24 21:33:25 -0500

    Add print statements into lightsources implement file.

--- orientation_reflectance/light_sources.cc
+++ orientation_reflectance/light_sources.cc
@@ -20,15 +20,15 @@ using namespace std;
 
 namespace Programs {
 
-  vector<int> FindLightSourceVector(Image* an_image, size_t centerX, size_t centerY, size_t radius) {
+  vector<int> FindLightSourceVector(Image* an_image, int centerX, int centerY, int radius) {
     vector<int> light_source;
 
     //find the brightest pixel location
     size_t maxBrightness = 0;
     //maxR is the Y position
-    size_t maxR = 0;
+    int maxR = 0;
     //maxC is the X position
-    size_t maxC = 0;
+    int maxC = 0;
     for(size_t i = 0; i < an_image->num_rows(); i++) {
     	for(size_t j = 0; j < an_image->num_columns(); j++) {
     		int pixel = an_image->GetPixel(i,j);
@@ -39,6 +39,7 @@ namespace Programs {
     		}
     	}
     }
+    cout << "Max Brightness: " << maxBrightness << endl;
 
     //Calculate the light source vector
     //which is the normal of the brightest pixel
@@ -49,10 +50,23 @@ namespace Programs {
     light_source.push_back(deltaY);
 
     //calculate the Z component of normal vector
-    size_t z_comp = 0;
+    int z_comp = 0;
     //R^2 - (X-Xc)^2 - (Y-Yc)^2
+    cout << "centerX: " << centerX << endl;
+    cout << "maxC: " << maxC << endl;
+    cout << "centerY: " << centerY << endl;
+    cout << "maxR: " << maxR << endl;
+    cout << "deltaX,deltaY: " << deltaX << ", " << deltaY << endl;
+    cout << pow(radius,2) << endl;
+    cout << pow(deltaX,2) << endl;
+    cout << pow(deltaY,2) << endl;
+    cout << "-------------" << endl;
     z_comp = sqrt(pow(radius,2)-pow(deltaX,2)-pow(deltaY,2));
     light_source.push_back(z_comp);
+
+
+    //TODO:scale the vector based off brightness magitutde....
+
 
     //Draw the vector onto the image?
     //first make a copy, then draw onto the copy!
